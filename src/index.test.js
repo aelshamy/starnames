da793043dd05413d15085ecdf8abe361bd9c3999
aelshamy <amr-hussien@hotmail.com>
2015-12-24 04:25:22 +0200

    feat(random): Add ability to get an array of starwars names
    If you pass a number to the random function, you will recieve an array with that number of random
    items
    
    closes #2

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,10 @@
 var expect = require('chai').expect
-var starwar = require('./index');
+var starWars = require('./index');
 
 describe("starnames", function() {
     describe("all", function() {
         it("should be an array of strings", function() {
-            expect(starwar.all).to.satisfy(isArrayOfStrings);
+            expect(starWars.all).to.satisfy(isArrayOfStrings);
 
             function isArrayOfStrings(array) {
                 return array.every(function(item) {
@@ -14,12 +14,12 @@ describe("starnames", function() {
         });
 
         it("should contain 'Luke Skywalker'", function() {
-            expect(starwar.all).to.include('Luke Skywalker');
+            expect(starWars.all).to.include('Luke Skywalker');
         });
 
         it("should have at least 5 names starts with 'J'", function() {
-        	var items = starwar.all.filter(function(item){
-        		return item.substring(0,1) === 'Z'
+        	var items = starWars.all.filter(function(item){
+        		return item.substring(0,1) === 'J'
         	})
         	expect(items).to.have.length.of.at.least(5);
         	
@@ -29,9 +29,17 @@ describe("starnames", function() {
     });
 
     describe("random", function() {
-        it("should return a random item from the starwar.all", function() {
-            var randomItem = starwar.random();
-            expect(starwar.all).to.include(randomItem);
+        it("should return a random item from the starWars.all", function() {
+            var randomItem = starWars.random();
+            expect(starWars.all).to.include(randomItem);
+        });
+        it("should return array of random items if passed a number", function() {
+            var randomItems = starWars.random(3);
+            expect(randomItems).to.have.length(3);
+
+            randomItems.forEach(function(item){
+                expect(starWars.all).to.include(item);
+            })
         });
 
     });
